2bad0898e6ddebd09a8bb762e724a4d6924d5697
Steven Gomez <brastegocha@gmail.com>
2025-12-03 21:27:33 -0500

    Fixing only one access point

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://faccstore.netlify.app",
   credentials: true
 }));
 app.use(cookieParser());
