3c0a771780a1cc6cf390f992e035fd01787cdd6a
Steven Gomez <brastegocha@gmail.com>
2025-12-25 12:57:20 -0500

    removing server.js from tracking

--- models/Product.js
+++ models/Product.js
@@ -36,8 +36,14 @@ const productSchema = new mongoose.Schema(
       type: Number,
       default: 0,
     },
+
     image:{
       type: String
+    },
+      category:{
+      type: String,
+            enum: ["men", "women", "kids", "babies", "misc"],
+            default: "misc",
     },
     createdBy: {
       type: mongoose.Schema.Types.ObjectId,
